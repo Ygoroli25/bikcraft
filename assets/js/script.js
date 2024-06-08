99ee8ad8eef3b81e34829cd974b99dec5bfba357
Ygoroli25 <ygor0419@gmail.com>
2024-06-08 14:53:44 -0300

    add parameters script to "orcamento" page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
+// ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
-const url = location.href;
-const href = link.href;
-if(url.includes(href)){
-  link.classList.add('ativo')
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
+links.forEach(ativarLink);
 
+// ativar itens do orçamentos
+const paramentos = new URLSearchParams(location.search);
+
+function ativarProduto(paramento) {
+  const elemento = document.getElementById(paramento);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 
-links.forEach(ativarLink)
+paramentos.forEach(ativarProduto);
