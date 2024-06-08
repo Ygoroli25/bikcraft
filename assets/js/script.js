4b8cb2a24b340aea258a99fd94e558662e63b0ba
Ygoroli25 <ygor0419@gmail.com>
2024-06-08 16:11:25 -0300

    add script in bicicleta-imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// galeria de imagens
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaConteiner = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width:940px)").matches;
+  if(media){
+  galeriaConteiner.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
